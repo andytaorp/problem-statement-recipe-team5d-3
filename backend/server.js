62e9997eae1fff8db0409d09b84a3f23aa0a0990
johnmiyahar <23001571@myrp.edu.sg>
2025-02-07 14:51:40 +0800

    commit

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ app.use(cors());
 app.use(express.json());
 require('dotenv').config(); 
 
-mongoose.connect('mongodb+srv://johnmiyahar:<db_password>@mernapp.ds7jm.mongodb.net/?retryWrites=true&w=majority&appName=MERNapp', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 app.post('/api/auth/signup', async (req, res) => {
   const { email, password } = req.body;
